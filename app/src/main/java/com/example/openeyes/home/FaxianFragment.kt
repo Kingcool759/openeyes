47bc28b8b16444e27ecb52dfb6bdebb8f9163337
zhaojianwei01 <3058587756@qq.com>
2020-10-25 15:45:41 +0800

    下拉刷新+上拉加载

--- app/src/main/java/com/example/openeyes/home/FaxianFragment.kt
+++ app/src/main/java/com/example/openeyes/home/FaxianFragment.kt
@@ -1,7 +1,6 @@
 package com.example.openeyes.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,10 @@ import com.example.openeyes.R
 import com.example.openeyes.adapter.HomeRecyclerAdapter
 import com.example.openeyes.databean.Item
 import com.example.openeyes.databinding.FaxianFragmentBinding
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
 import kotlinx.android.synthetic.main.faxian_fragment.*
+
 
 class FaxianFragment : Fragment() {
     private lateinit var binding: FaxianFragmentBinding
@@ -38,7 +40,7 @@ class FaxianFragment : Fragment() {
 
         viewModel.getfaxianList()
         getDataCallback()
-
+        setSmartRefreshLayout()
     }
 
     private fun getDataCallback() {
@@ -56,6 +58,21 @@ class FaxianFragment : Fragment() {
         home_recycler.layoutManager = layoutManager
         context?.let {
             home_recycler.adapter = HomeRecyclerAdapter(it, dataList)
+        }
+    }
+    //设置SmartRefreshLayout
+    private fun setSmartRefreshLayout(){
+        refreshLayout.setRefreshHeader(ClassicsHeader(context)); //经典头
+        refreshLayout.setRefreshFooter(ClassicsFooter(context)); //经典尾
+//        refreshLayout.setRefreshHeader(BezierRadarHeader(context).setEnableHorizontalDrag(true)) //雷达刷新头
+        //        refreshLayout.setRefreshHeader(new MaterialHeader(this));//谷歌刷新头
+//        refreshLayout.setRefreshHeader(new TwoLevelHeader(this));//二级刷新头
+//        refreshLayout.setRefreshFooter(BallPulseFooter(context).setSpinnerStyle(SpinnerStyle.Scale)) //脉冲尾
+        refreshLayout.setOnRefreshListener { refreshlayout ->
+            refreshlayout.finishRefresh(2000 /*,false*/) //传入false表示刷新失败
+        }
+        refreshLayout.setOnLoadMoreListener { refreshlayout ->
+            refreshlayout.finishLoadMore(2000 /*,false*/) //传入false表示加载失败
         }
     }
 }
